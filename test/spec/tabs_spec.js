6f9e3ec5870bba541a4cec856287dd417dc21272
Kai Culemann <culemann@omniscale.de>
2013-02-08 09:46:42 +0100

    Add tests for pane/tab toggle-group/toggle-element directives

---
+++ test/spec/tabs_spec.js
@@ -0,0 +1,154 @@
+describe('tabs', function() {
+    var elm, scope;
+
+    // load the tabs code
+    beforeEach(module('mapproxy_gui.directives'));
+
+    // prepare testable element
+    beforeEach(inject(function($rootScope, $compile) {
+        elm = angular.element(
+            '<div>' +
+                '<tabs>' +
+                    '<pane title="First Tab">' +
+                        'first content is {{first}}' +
+                    '</pane>' +
+                    '<pane title="Second Tab">' +
+                        'second content is {{second}}' +
+                    '</pane>' +
+                '</tabs>' +
+            '</div>');
+
+        scope = $rootScope;
+        $compile(elm)(scope);
+        scope.$digest();
+    }));
+
+    it('should create clickable titles', inject(function($rootScope, $compile) {
+        var titles = elm.find('ul.nav-tabs li a');
+
+        expect(titles.length).toBe(2);
+        expect(titles.eq(0).text()).toBe('First Tab');
+        expect(titles.eq(1).text()).toBe('Second Tab');
+    }));
+
+    it('should bind the content', function() {
+        var contents = elm.find('div.tab-content div.tab-pane');
+
+        expect(contents.length).toBe(2);
+        expect(contents.eq(0).text()).toBe('first content is ');
+        expect(contents.eq(1).text()).toBe('second content is ');
+
+        scope.$apply(function() {
+            scope.first = 123;
+            scope.second = 456;
+        });
+
+        expect(contents.eq(0).text()).toBe('first content is 123');
+        expect(contents.eq(1).text()).toBe('second content is 456');
+    });
+
+    it('should set active class on title', function() {
+        var titles = elm.find('ul.nav-tabs li');
+
+        expect(titles.eq(0)).toHaveClass('active');
+        expect(titles.eq(1)).not.toHaveClass('active');
+    });
+
+
+    it('should set active class on content', function() {
+        var contents = elm.find('div.tab-content div.tab-pane');
+
+        expect(contents.eq(0)).toHaveClass('active');
+        expect(contents.eq(1)).not.toHaveClass('active');
+    });
+
+
+    it('should change active pane when title clicked', function() {
+        var titles = elm.find('ul.nav-tabs li');
+        var contents = elm.find('div.tab-content div.tab-pane');
+
+        // click the second tab
+        titles.eq(1).find('a').click();
+
+        // second title should be active
+        expect(titles.eq(0)).not.toHaveClass('active');
+        expect(titles.eq(1)).toHaveClass('active');
+
+        // second content should be active
+        expect(contents.eq(0)).not.toHaveClass('active');
+        expect(contents.eq(1)).toHaveClass('active');
+    });
+});
+
+describe('tabs controller', function() {
+  var scope, ctrl;
+
+  beforeEach(inject(function($controller, $rootScope) {
+    scope = $rootScope;
+
+    // instantiate the controller stand-alone, without the directive
+    ctrl = $controller(tabsCtrl, {$scope: scope, $element: null});
+  }));
+
+
+  describe('select', function() {
+
+    it('should mark given pane selected', function() {
+      var pane = {};
+
+      scope.select(pane);
+      expect(pane.selected).toBe(true);
+    });
+
+
+    it('should deselect other panes', function() {
+      var pane1 = {}, pane2 = {}, pane3 = {};
+
+      ctrl.addPane(pane1);
+      ctrl.addPane(pane2);
+      ctrl.addPane(pane3);
+
+      scope.select(pane1);
+      expect(pane1.selected).toBe(true);
+      expect(pane2.selected).toBe(false);
+      expect(pane3.selected).toBe(false);
+
+      scope.select(pane2);
+      expect(pane1.selected).toBe(false);
+      expect(pane2.selected).toBe(true);
+      expect(pane3.selected).toBe(false);
+
+      scope.select(pane3);
+      expect(pane1.selected).toBe(false);
+      expect(pane2.selected).toBe(false);
+      expect(pane3.selected).toBe(true);
+    });
+  });
+
+
+  describe('addPane', function() {
+
+    it('should append pane', function() {
+      var pane1 = {}, pane2 = {};
+
+      expect(scope.panes).toEqual([]);
+
+      ctrl.addPane(pane1);
+      expect(scope.panes).toEqual([pane1]);
+
+      ctrl.addPane(pane2);
+      expect(scope.panes).toEqual([pane1, pane2]);
+    });
+
+
+    it('should select the first one', function() {
+      var pane1 = {}, pane2 = {};
+
+      ctrl.addPane(pane1);
+      expect(pane1.selected).toBe(true);
+
+      ctrl.addPane(pane2);
+      expect(pane1.selected).toBe(true);
+    });
+  });
+});
---
+++ test/spec/toggle_spec.js
@@ -0,0 +1,240 @@
+/* what to test
+
+    - default behavior
+    - toggle modes
+    - toggle-element types
+
+    notes:
+    default toggle element mode = self
+    default toggle-group mode = hideOther
+*/
+
+describe('toggleElement hideOther', function() {
+    var elm, scope;
+
+    // load the toggleElement code
+    beforeEach(module('mapproxy_gui.directives'));
+
+    describe('default mode (toggle element)', function() {
+        beforeEach(inject(function($rootScope, $compile) {
+            elm = angular.element(
+                '<toggle-group>' +
+                    '<div ng-show="true" toggle-element>Elem1</div>' +
+                    '<div ng-show="false" toggle-element>Elem2</div>' +
+                    '<div ng-show="false" toggle-element>Elem3</div>' +
+                '</toggle-group>');
+            scope = $rootScope;
+            $compile(elm)(scope);
+            scope.$digest();
+        }));
+
+        it('should show only the first element', function() {
+            var toggle_elements = elm.find('div');
+
+            expect(toggle_elements.length).toBe(3);
+            expect(toggle_elements.eq(0).text()).toBe('Elem1');
+            expect(toggle_elements.eq(1).text()).toBe('Elem2');
+            expect(toggle_elements.eq(2).text()).toBe('Elem3');
+
+            expect(toggle_elements.eq(0).css('display')).toBe('block');
+            expect(toggle_elements.eq(1).css('display')).toBe('none');
+            expect(toggle_elements.eq(2).css('display')).toBe('none');
+        });
+
+        it('should show only the secound element after click it', function() {
+            var toggle_elements = elm.find('div');
+
+            expect(toggle_elements.length).toBe(3);
+            expect(toggle_elements.eq(0).text()).toBe('Elem1');
+            expect(toggle_elements.eq(1).text()).toBe('Elem2');
+            expect(toggle_elements.eq(2).text()).toBe('Elem3');
+
+            toggle_elements.eq(1).click();
+
+            expect(toggle_elements.eq(0).css('display')).toBe('none');
+            expect(toggle_elements.eq(1).css('display')).toBe('block');
+            expect(toggle_elements.eq(2).css('display')).toBe('none');
+
+        });
+    });
+
+    describe('toggle next element', function() {
+        beforeEach(inject(function($rootScope, $compile) {
+            elm = angular.element(
+                '<toggle-group>' +
+                    '<h3 toggle-element="next">Heading Elem1</h3>' +
+                    '<div ng-show="true">Elem1</div>' +
+                    '<h3 toggle-element="next">Heading Elem2</h3>' +
+                    '<div ng-show="false">Elem2</div>' +
+                    '<h3 toggle-element="next">Heading Elem3</h3>' +
+                    '<div ng-show="false">Elem3</div>' +
+                '</toggle-group>');
+            scope = $rootScope;
+            $compile(elm)(scope);
+            scope.$digest();
+        }));
+
+        it('should show all headings and first heading next element', function() {
+            var headings = elm.find('h3');
+            var contents = elm.find('div');
+
+            expect(headings.length).toBe(3);
+            expect(headings.eq(0).text()).toBe('Heading Elem1');
+            expect(headings.eq(1).text()).toBe('Heading Elem2');
+            expect(headings.eq(2).text()).toBe('Heading Elem3');
+
+            expect(headings.eq(0).css('display')).toBe('block');
+            expect(headings.eq(1).css('display')).toBe('block');
+            expect(headings.eq(2).css('display')).toBe('block');
+
+            expect(contents.length).toBe(3);
+            expect(contents.eq(0).text()).toBe('Elem1');
+            expect(contents.eq(1).text()).toBe('Elem2');
+            expect(contents.eq(2).text()).toBe('Elem3');
+
+            expect(contents.eq(0).css('display')).toBe('block');
+            expect(contents.eq(1).css('display')).toBe('none');
+            expect(contents.eq(2).css('display')).toBe('none');
+        });
+
+        it('should show all headings and secound heading next element after click secound heading', function() {
+            var headings = elm.find('h3');
+            var contents = elm.find('div');
+
+            expect(headings.length).toBe(3);
+            expect(headings.eq(0).text()).toBe('Heading Elem1');
+            expect(headings.eq(1).text()).toBe('Heading Elem2');
+            expect(headings.eq(2).text()).toBe('Heading Elem3');
+
+            expect(headings.eq(0).css('display')).toBe('block');
+            expect(headings.eq(1).css('display')).toBe('block');
+            expect(headings.eq(2).css('display')).toBe('block');
+
+            expect(contents.length).toBe(3);
+            expect(contents.eq(0).text()).toBe('Elem1');
+            expect(contents.eq(1).text()).toBe('Elem2');
+            expect(contents.eq(2).text()).toBe('Elem3');
+
+            headings.eq(1).click();
+
+            expect(contents.eq(0).css('display')).toBe('none');
+            expect(contents.eq(1).css('display')).toBe('block');
+            expect(contents.eq(2).css('display')).toBe('none');
+        });
+    });
+});
+
+describe('toggleElement multiShow', function() {
+    var elm, scope;
+
+    // load the toggleElement code
+    beforeEach(module('mapproxy_gui.directives'));
+
+    describe('default mode (toggle element)', function() {
+        beforeEach(inject(function($rootScope, $compile) {
+            elm = angular.element(
+                '<toggle-group mode="multiShow">' +
+                    '<div ng-show="true" toggle-element>Elem1</div>' +
+                    '<div ng-show="false" toggle-element>Elem2</div>' +
+                    '<div ng-show="false" toggle-element>Elem3</div>' +
+                '</toggle-group>');
+            scope = $rootScope;
+            $compile(elm)(scope);
+            scope.$digest();
+        }));
+
+        it('should show only the first element', function() {
+            var toggle_elements = elm.find('div');
+
+            expect(toggle_elements.length).toBe(3);
+            expect(toggle_elements.eq(0).text()).toBe('Elem1');
+            expect(toggle_elements.eq(1).text()).toBe('Elem2');
+            expect(toggle_elements.eq(2).text()).toBe('Elem3');
+
+            expect(toggle_elements.eq(0).css('display')).toBe('block');
+            expect(toggle_elements.eq(1).css('display')).toBe('none');
+            expect(toggle_elements.eq(2).css('display')).toBe('none');
+        });
+
+        it('should show first and secound element after click it', function() {
+            var toggle_elements = elm.find('div');
+
+            expect(toggle_elements.length).toBe(3);
+            expect(toggle_elements.eq(0).text()).toBe('Elem1');
+            expect(toggle_elements.eq(1).text()).toBe('Elem2');
+            expect(toggle_elements.eq(2).text()).toBe('Elem3');
+
+            toggle_elements.eq(1).click();
+
+            expect(toggle_elements.eq(0).css('display')).toBe('block');
+            expect(toggle_elements.eq(1).css('display')).toBe('block');
+            expect(toggle_elements.eq(2).css('display')).toBe('none');
+
+        });
+    });
+
+    describe('toggle next element', function() {
+        beforeEach(inject(function($rootScope, $compile) {
+            elm = angular.element(
+                '<toggle-group mode="multiShow">' +
+                    '<h3 toggle-element="next">Heading Elem1</h3>' +
+                    '<div ng-show="true">Elem1</div>' +
+                    '<h3 toggle-element="next">Heading Elem2</h3>' +
+                    '<div ng-show="false">Elem2</div>' +
+                    '<h3 toggle-element="next">Heading Elem3</h3>' +
+                    '<div ng-show="false">Elem3</div>' +
+                '</toggle-group>');
+            scope = $rootScope;
+            $compile(elm)(scope);
+            scope.$digest();
+        }));
+
+        it('should show all headings and first heading next element', function() {
+            var headings = elm.find('h3');
+            var contents = elm.find('div');
+
+            expect(headings.length).toBe(3);
+            expect(headings.eq(0).text()).toBe('Heading Elem1');
+            expect(headings.eq(1).text()).toBe('Heading Elem2');
+            expect(headings.eq(2).text()).toBe('Heading Elem3');
+
+            expect(headings.eq(0).css('display')).toBe('block');
+            expect(headings.eq(1).css('display')).toBe('block');
+            expect(headings.eq(2).css('display')).toBe('block');
+
+            expect(contents.length).toBe(3);
+            expect(contents.eq(0).text()).toBe('Elem1');
+            expect(contents.eq(1).text()).toBe('Elem2');
+            expect(contents.eq(2).text()).toBe('Elem3');
+
+            expect(contents.eq(0).css('display')).toBe('block');
+            expect(contents.eq(1).css('display')).toBe('none');
+            expect(contents.eq(2).css('display')).toBe('none');
+        });
+
+        it('should show all headings, first and secound headings next element after click secound heading', function() {
+            var headings = elm.find('h3');
+            var contents = elm.find('div');
+
+            expect(headings.length).toBe(3);
+            expect(headings.eq(0).text()).toBe('Heading Elem1');
+            expect(headings.eq(1).text()).toBe('Heading Elem2');
+            expect(headings.eq(2).text()).toBe('Heading Elem3');
+
+            expect(headings.eq(0).css('display')).toBe('block');
+            expect(headings.eq(1).css('display')).toBe('block');
+            expect(headings.eq(2).css('display')).toBe('block');
+
+            expect(contents.length).toBe(3);
+            expect(contents.eq(0).text()).toBe('Elem1');
+            expect(contents.eq(1).text()).toBe('Elem2');
+            expect(contents.eq(2).text()).toBe('Elem3');
+
+            headings.eq(1).click();
+
+            expect(contents.eq(0).css('display')).toBe('block');
+            expect(contents.eq(1).css('display')).toBe('block');
+            expect(contents.eq(2).css('display')).toBe('none');
+        });
+    });
+});
