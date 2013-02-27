ee68b17c5bb5f2b011dd023492dbb266487946cb
Kai Culemann <culemann@omniscale.de>
2013-02-27 16:50:06 +0100

    Improved helper

--- app/mapproxy_webconf/static/js/helper.js
+++ app/mapproxy_webconf/static/js/helper.js
@@ -10,11 +10,11 @@ var clearData = function(dict) {
     angular.forEach(angular.copy(dict), function(value, key) {
         if(typeof(value) == 'boolean') {
             angular.noop();
-        } else if(value == undefined || value == 0 || value == [] || value == {} || value == null) {
+        } else if(value == undefined || value == 0 || value == [] || value == {} || value == null || value == '') {
             delete(dict[key]);
         } else if(angular.isArray(value)) {
             angular.forEach(angular.copy(value), function(val) {
-                if(val == undefined || val == 0 || val == [] || val == {} || val == null) {
+                if(val == undefined || val == 0 || val == [] || val == {} || val == null || value == '') {
                     value.splice(value.indexOf(val), 1);
                 }
             });
