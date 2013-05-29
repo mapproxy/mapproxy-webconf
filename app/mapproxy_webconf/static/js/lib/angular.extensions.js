907e7f0c3a0a7bd0167d52e127b183fb771ea141
Kai Culemann <culemann@omniscale.de>
2013-05-29 12:08:17 +0200

    Add angular.extensions.js

---
+++ app/mapproxy_webconf/static/js/lib/angular.extensions.js
@@ -0,0 +1,25 @@
+angular.module('angular.extensions', [])
+
+//found at https://gist.github.com/eliotsykes/5394631
+//remove when https://github.com/angular/angular.js/issues/1277 is closed and included into angularjs
+.directive('ngFocus', ['$parse', function($parse) {
+  return function(scope, element, attr) {
+    var fn = $parse(attr['ngFocus']);
+    element.bind('focus', function(event) {
+      scope.$apply(function() {
+        fn(scope, {$event:event});
+      });
+    });
+  }
+}])
+
+.directive('ngBlur', ['$parse', function($parse) {
+  return function(scope, element, attr) {
+    var fn = $parse(attr['ngBlur']);
+    element.bind('blur', function(event) {
+      scope.$apply(function() {
+        fn(scope, {$event:event});
+      });
+    });
+  }
+}]);
