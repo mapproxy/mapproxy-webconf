09af3a4c03a2f9b167620a71e4ec48a2b3568953
Kai Culemann <culemann@omniscale.de>
2013-06-06 08:59:55 +0200

    Make tooltips working with i18n filter

--- app/mapproxy_webconf/static/js/directives.js
+++ app/mapproxy_webconf/static/js/directives.js
@@ -629,25 +629,55 @@ directive('editarea', function($http, MessageService) {
 directive('tooltip', function() {
     return {
         restrict: 'A',
+        scope: 'element',
         link: function(scope, element, attrs) {
-            scope.tooltipMode = attrs.tooltip || 'tooltip';
-            scope.tooltipContent = attrs.tooltipContent;
-            scope.tooltipTitle = attrs.tooltipTitle;
-            scope.tooltipPlacement = attrs.tooltipPlacement || 'right';
-            if(scope.tooltipMode == 'popover') {
+            var initPopover = function() {
                 $(element).popover({
                     title: scope.tooltipTitle,
                     content: scope.tooltipContent,
                     trigger: 'hover',
                     placement: scope.tooltipPlacement
                 });
-            } else {
+            };
+            var initTooltip = function() {
                 $(element).attr('data-original-title', scope.tooltipContent);
                 $(element).tooltip({
                     title: scope.tooltipTitle,
                     placement: scope.tooltipPlacement
                 });
+            };
+
+            scope.tooltipMode = attrs.tooltip || 'tooltip';
+            scope.tooltipContent = attrs.tooltipContent;
+            scope.tooltipTitle = attrs.tooltipTitle;
+            scope.tooltipPlacement = attrs.tooltipPlacement || 'right';
+
+            if(scope.tooltipMode == 'popover') {
+                initPopover();
+            } else {
+                initTooltip();
             }
+
+            attrs.$observe('tooltipContent', function(val) {
+                scope.tooltipContent = val;
+                if(scope.tooltipMode == 'popover') {
+                    $(element).popover('destroy');
+                    initPopover();
+                } else {
+                    $(element).tooltip('destroy');
+                    initTooltip();
+                }
+            });
+            attrs.$observe('tooltipTitle', function(val) {
+                scope.tooltipTitle = val;
+                if(scope.tooltipMode == 'popover') {
+                    $(element).popover('destroy');
+                    initPopover();
+                } else {
+                    $(element).tooltip('destroy');
+                    initTooltip();
+                }
+            });
         }
     }
 }).
