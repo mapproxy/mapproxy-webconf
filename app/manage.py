6a3d8aeb34e875b34e1b3794ccda1425b18b9044
Kai Culemann <culemann@omniscale.de>
2013-06-12 14:07:14 +0200

    Add extract command

--- app/manage.py
+++ app/manage.py
@@ -1,5 +1,6 @@
 import sys
 import scriptine
+from scriptine.shell import sh
 from mapproxy_webconf.app import init_app
 
 def runserver_command(host='localhost', port=8080):
@@ -7,5 +8,8 @@ def runserver_command(host='localhost', port=8080):
     app = init_app('./')
     app.run(host=host, port=port, debug=True, reloader=True)
 
+def extract_command():
+    "Extract messages"
+    sh('pygettext -a -v -d messages -o mapproxy_webconf/locale/messages.pot mapproxy_webconf/\*.py mapproxy_webconf/templates/\*.html mapproxy_webconf/ static/angular_templates/\*.html')
 if __name__ == '__main__':
     scriptine.run()
