36cb9ffde28ef49369723c805cdf71cdbdb903de
Kai Culemann <culemann@omniscale.de>
2013-06-20 09:44:47 +0200

    Put PROJECT into rootScope

--- app/mapproxy_webconf/static/js/resources.js
+++ app/mapproxy_webconf/static/js/resources.js
@@ -1,7 +1,7 @@
 angular.module('mapproxy_gui.resources', ['ngResource']).
 
-factory('MapproxyResource', function($resource) {
-    return $resource('/conf/'+PROJECT+'/:action/:id', {id: '@id', action: '@action'}, {
+factory('MapproxyResource', function($resource, $rootScope) {
+    return $resource('/conf/'+$rootScope.PROJECT+'/:action/:id', {id: '@id', action: '@action'}, {
         update: {method: 'PUT'}
     });
 });
