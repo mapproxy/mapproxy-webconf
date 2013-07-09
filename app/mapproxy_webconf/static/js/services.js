69696fcb8fe2a77f684e0f4e212cdaec8d9f8441
Kai Culemann <culemann@omniscale.de>
2013-07-09 14:41:38 +0200

    Return deep extended item

--- app/mapproxy_webconf/static/js/services.js
+++ app/mapproxy_webconf/static/js/services.js
@@ -141,7 +141,7 @@ var MapproxyBaseService = function(_section, _model, _preloadedData) {
             if(angular.isDefined(_this._rootScope))
                 _this._rootScope.$broadcast(_this._section + '.current');
         } else {
-            return angular.copy($.extend({}, {'data': _this._model}, _this._item));
+            return angular.copy($.extend(true, {}, {'data': _this._model}, _this._item));
         }
     };
     this.last = function() {
