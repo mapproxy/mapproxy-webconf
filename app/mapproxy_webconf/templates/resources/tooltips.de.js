a0d61704e760446a3a3e8fa2fd29497f57bd0dc1
Dominik Helle <helle@omniscale.de>
2013-07-10 07:52:34 +0200

    add tooltips on caches

--- app/mapproxy_webconf/templates/resources/tooltips.de.js
+++ app/mapproxy_webconf/templates/resources/tooltips.de.js
@@ -3,19 +3,19 @@
         "content": "Diese Option wird in den meisten Fällen nicht benötigt"
     },
     "filter": {
-        "content": "Mit dem Filter können Sie die Auswahl ihrer angelegten Sources einschränken."
+        "content": "Über den Filter können Sie die Auswahl Ihrer Elemente einschränken."
     },
     "source_available_wms": {
         "content": "Fügen Sie hier vorhanden WMS-Dienste hinzu um diese als Source für den MapProxy verwenden zu können. Nach dem Hinzufügen können Sie die per Drag & Drop zur Sources ziehen. <br><br>Über die <strong>Weltkugel</strong> haben Sie die Möglichkeit sich den Dienst anzuschauen."
     },
     "sources_name": {
-        "content": "Geben Sie hier den Namen der Quelle ein. Dieser dient später zur Eindeutigen Identfikation."
+        "content": "Geben Sie hier den Namen der Quelle ein. Dieser dient später zur Eindeutigen Identifikation."
     },
     "sources_url":{
         "content": "Über die URL der Quelle wird der Dienst abgefragt. Diese können Sie per Hand eingeben oder per Drag & Drop aus dem rechts verfügbaren WMS hinzufügen."
     },
     "sources_layers": {
-        "content": "Definieren Sie hier die Layer die von MapProxy abgerurfen werden sollen. Die Layer werden in der unten angegebenen Reihenfolge – von unten nach oben – abgefragt."
+        "content": "Definieren Sie hier die Layer die von MapProxy abgerufen werden sollen. Die Layer werden in der unten angegebenen Reihenfolge – von unten nach oben – abgefragt."
     },
     "sources_manual_add_layers": {
         "content": "Fügen Sie Layer manuell hinzu. \n Dies ist zum Beispiel notwendig wenn der Original-WMS kein Capabilites-Dokument ausliefert."
@@ -24,13 +24,13 @@
         "content": "Unterstützt Ihre Sources nur ein bestimmtes Bildformat? Dann können Sie dieses hier einstellen. Falls keine Einstellung vorgenommen wird, wird das Format des Caches verwendet."
     },
     "sources_add_wms": {
-        "content": "Fügen Sie hier die URL Ihres Quell-Dienstes ein. Das System fragt das Capabilites-Dokument ab und zeigt Ihnen die vorhandnen Informationen an. Diese können Sie dann per Drag & Drop in die Source ziehen."
+        "content": "Fügen Sie hier die URL Ihres Quell-Dienstes ein. Das System fragt das Capabilities-Dokument ab und zeigt Ihnen die vorhandnen Informationen an. Diese können Sie dann per Drag & Drop in die Source ziehen."
     },
     "sources_coverage": {
         "content": "Über die Angabe eines Coverages können Sie die Quelle auf einen bestimmten Bereich begrenzen. Erstellen Sie eine Bounding Box oder Zeichnen Sie ein Polygon in der Karte."
     },
     "sources_supported_srs": {
-        "content": "Unterstüzt die Quelle nicht die angefragten Koordinatensystem können Sie hier gezielt einstellen, welches Koordinatensystem angefragt werden soll. In vielen Fällen ist dies aber nicht nötig. Ist kein Koordinatensystem ausgewählt wird das im  Cache definerte verwendet."
+        "content": "Unterstüzt die Quelle nicht die angefragte Koordinatensystem können Sie hier gezielt einstellen welches Koordinatensystem angefragt werden soll. In vielen Fällen ist dies aber nicht nötig. Ist kein Koordinatensystem ausgewählt wird das im  Cache definerte verwendet."
     },
     "min_max_res": {
         "content": "Stellen Sie ein von ab und bis zu welchem Maßstab oder Auflösung die Einstellungen gelten. Sie können bei der eingabe zwischen Maßstab und Auflösung, sowie zwischen Grad und Meter auswählen."
@@ -49,6 +49,33 @@
     },
     "sources_add_srs_manual": {
         "content": "Fügen Sie hier die SRS manuell hinzu. Bitte geben Sie hier den EPSG-Code an. Zum Beispiel: EPSG:31467"
+    },
+    "cache_name": {
+        "content": "Geben Sie den Namen des Caches ein. Dieser dient später zur eindeutigen Identifikation. Bitte beachten Sie das ein Cache nicht den selben Namen tragen kann wie eine Source."
+    },
+    "cache_sources": {
+        "content": "Fügen Sie per Drag&Drop die Sources oder Caches zu die verwendet werden sollen."
+    },
+    "cache_grids": {
+        "content": "Definieren Sie die Grids die verwendet werden sollen. Sie können diese per Drag & Drop von rechts in das Feld ziehen."
+    },
+    "cache_format": {
+        "content": "Wählen Sie das Bildformat des Caches sowie das vom MapProxy angefragte Bildformat aus."
+    },
+    "cache_image_format": {
+        "content": "Über den Parameter 'Format' können sie das Bildformat für die Daten im Cache einstellen. Standardmässig verwendet MapProxy PNG."
+    },
+    "cache_request_format": {
+        "content": "Definieren Sie hier in welchem Bildformat die Daten für den Cache angefragt werden soll. Standardmässig wird das unter Format des Caches verwendet. Häufig muss diese Option daher nicht genutzt werden. "
+    },
+    "cache_sources_list": {
+        "content": "Ziehen Sie die gewünschte Sources per Drag & Drop in den Bereich <em>Sources</em> im ausgewählten Cache."
+    },
+    "cache_caches_list": {
+        "content": "Auch Caches können als Quelle für Caches verwendet werden. Ziehen Sie den gewünchsten Cache in den Bereich Sources"
+    },
+    "cache_grids_list": {
+        "content": "Wählen Sie das passende Grid für Ihren Cache aus. Falls Grids im nachhinein verändert werden muss der Cache unter umständen geändert werden. Nutzen Sie daher, wenn möglich, nur fertige Grids."
     }
 }
 
