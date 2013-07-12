16393c80ca79b247d6b772bb155a60c11473be27
Kai Culemann <culemann@omniscale.de>
2013-07-12 15:18:39 +0200

    Don't pass transformed grid to tile_bbox

--- app/mapproxy_webconf/lib/geojson.py
+++ app/mapproxy_webconf/lib/geojson.py
@@ -48,7 +48,7 @@ class ConfigGeoJSONGrid(object):
         except TypeError:
             self.level = None
 
-        self.tilegrid = tile_grid(srs=self.grid_srs, bbox=self.grid_bbox, bbox_srs=self.grid_bbox_srs, origin=self.origin, res=self.res, num_levels=self._num_levels)
+        self.tilegrid = tile_grid(srs=self.grid_srs, bbox=_grid_bbox, bbox_srs=self.grid_bbox_srs, origin=self.origin, res=self.res, num_levels=self._num_levels)
         self.validate_tile_bbox_for_level_0()
 
     def transform_grid_bbox(self, _grid_bbox=None):
